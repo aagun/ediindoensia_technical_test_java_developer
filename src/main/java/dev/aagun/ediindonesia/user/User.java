9491cc7a7957d7ccfd6101772e641777803487ba
= <agun@duck.com>
2024-02-26 03:42:34 +0700

    Remove user credential model

--- src/main/java/dev/aagun/ediindonesia/user/User.java
+++ src/main/java/dev/aagun/ediindonesia/user/User.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -36,6 +35,4 @@ public class User {
 
     private char status;
 
-    @OneToOne(mappedBy = "user")
-    private UserCredential userCredential;
 }
